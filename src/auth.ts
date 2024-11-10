064cfa0254b5a15e835d7900394a4b6e5ff4e430
secona <77039267+secona@users.noreply.github.com>
2024-11-10 14:39:15 +0700

    feat: auth templates

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,25 @@
-/*
- * /auth
- * /auth/login
- * /auth/register
- * /auth/register/pekerja
- * /auth/register/pengguna
- */
+import express from 'express';
+
+const app = express.Router();
+
+app.get('/', (req, res) => {
+	res.render('auth/main');
+});
+
+app.get('/login', (req, res) => {
+	res.render('auth/login');
+});
+
+app.get('/register', (req, res) => {
+	res.render('auth/register/main');
+});
+
+app.get('/register/pekerja', (req, res) => {
+	res.render('auth/register/pekerja');
+});
+
+app.get('/register/pengguna', (req, res) => {
+	res.render('auth/register/pengguna');
+});
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,16 @@ import express from 'express';
 import path from 'path';
 import { engine } from 'express-handlebars';
 
+import auth from './auth';
+
 const app = express();
 
 app.engine('.hbs', engine({ extname: '.hbs' }));
 app.set('view engine', '.hbs');
 app.set('views', path.resolve(process.cwd(), 'templates'));
 
-app.get('/', (_req, res) => {
-	res.render('main', { name: 'World' });
-})
+app.use('/auth', auth);
 
-app.listen(3000, () => console.log('http://localhost:3000'))
+app.get('/', (_req, res) => res.render('main', { name: 'World' }));
+
+app.listen(3000, () => console.log('http://localhost:3000'));
