6357d36c44fa8dcdabdb9927e03ce52074eac06d
yasin <yasinmahendra26@gmail.com>
2024-11-17 20:33:54 +0700

    feat: make the homepage

--- src/home.ts
+++ src/home.ts
@@ -2,8 +2,48 @@ import express from 'express';
 
 const app = express.Router();
 
+const dataForHome = {
+    "category" : [
+        {
+            "name": "Home Cleaning",
+            "subcategory": [
+                "Daily Cleaning",
+                "Pembersihan Dapur dan Kulkas",
+            ],
+        },
+        {
+            "name": "Deep Cleaning",
+            "subcategory": [
+                "Cuci Kasur",
+                "Bersih Kamar Mandi",
+            ],
+        },
+        {
+            "name": "Plumbing Services",
+            "subcategory": [
+                "Perbaikan pipa bocor",
+                "Pembersihan Saluran Air",
+            ],
+        },
+        {
+            "name": "Electrical Repairs",
+            "subcategory": [
+                "Instalasi Lampu",
+                "Perbaikan Saklar dan Stop Kontak",
+            ],
+        },
+        {
+            "name": "Pest Control",
+            "subcategory": [
+                "Pengendalian Tikus",
+                "Pembasmian Kutu",
+            ],
+        },
+    ],
+};
+
 app.get('/', (req, res) => {
-    res.render('home/home.hbs');
+    res.render('home/home.hbs', { data: JSON.stringify(dataForHome), han : dataForHome });
 });
 
 export default app;
