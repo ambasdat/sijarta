66f4d1bf9ed00b2d63b53233f7583c377c9c7841
secona <77039267+secona@users.noreply.github.com>
2024-11-18 09:15:45 +0700

    feat: form registrasi errors

--- src/auth.ts
+++ src/auth.ts
@@ -25,8 +25,26 @@ app.get("/register/pekerja", (req, res) => {
   res.render("auth/register/pekerja");
 });
 
+app.post("/register/pekerja", (req, res) => {
+  const phoneExists = req.body.phone === "123";
+  const bankExists = req.body.bank === "OVO" && req.body.norek === "123";
+  const npwpExists = req.body.npwp === "123";
+
+  if (phoneExists || bankExists || npwpExists)
+    res.render("auth/register/pekerja", { phoneExists, bankExists, npwpExists })
+  else
+    res.redirect("/auth/login");
+})
+
 app.get("/register/pengguna", (req, res) => {
   res.render("auth/register/pengguna");
+});
+
+app.post("/register/pengguna", (req, res) => {
+  if (req.body.phone === "123")
+    res.render("auth/register/pengguna", { phoneExists: true })
+  else
+    res.redirect("/auth/login")
 });
 
 app.post("/logout", (req, res) => {
