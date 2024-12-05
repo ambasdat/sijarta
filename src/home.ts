a8d874ab82f95d3b3f00bace579a251cf2c249e6
yasin <yasinmahendra26@gmail.com>
2024-12-05 22:26:51 +0700

    add: a /home/nyoba for debugging routing to subkategori

--- src/home.ts
+++ src/home.ts
@@ -41,4 +41,21 @@ app.get("/", async (req, res) => {
   }
 });
 
+app.get("/nyoba", async (req, res) => {
+  try {
+    const s = req.query.s as string || "";
+    const k = req.query.k as string || "";
+    const result = await client.query(`SELECT * FROM getAllForHome($1, $2);`, [s, k]);
+    const NamaKategori = await client.query(`SELECT "NamaKategori" FROM "KATEGORI_JASA";`);
+    const data = parseQueryForHome(result.rows);
+    // const isGuest = req.userType === "guest";
+    const isGuest = false;
+    res.render("home/home.hbs", {data: data, guest: isGuest, NamaKategori: NamaKategori.rows});
+  } 
+  catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred while fetching data.");
+  }
+});
+
 export default app;
