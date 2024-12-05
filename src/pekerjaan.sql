2b39c3cdd17096710dde69b6ce046d820cbd13ff
adlynfakhreyz <adlynuniacc@gmail.com>
2024-12-05 13:34:06 +0700

    feat: implement filter status (note: unhandled update-status)

--- src/pekerjaan.sql
+++ src/pekerjaan.sql
@@ -1,6 +1,7 @@
-
 -- get_pekerja_category
-CREATE OR REPLACE FUNCTION get_pekerja_category(pekerjaId_param UUID)
+CREATE OR REPLACE FUNCTION get_pekerja_category(
+    pekerjaId_param UUID
+)
 RETURNS TABLE (
     kId UUID,      -- Category ID
     kName VARCHAR,  -- Category Name
@@ -26,6 +27,8 @@ END;
 $$ LANGUAGE plpgsql;
 
 
+
+
 -- filter_pemesanan
 CREATE OR REPLACE FUNCTION filter_pemesanan(
     pekerjaId_param UUID, 
@@ -36,18 +39,9 @@ RETURNS TABLE (
     IdTrPemesanan UUID,
     TglPemesanan DATE,
     TglPekerjaan DATE,
-    WaktuPekerjaan TIMESTAMP,
     TotalBiaya DECIMAL,
-    IdPelanggan UUID,
     NamaPelanggan VARCHAR,
-    IdPekerja UUID,
-    IdKategoriJasa UUID,
-    NamaSubkategoriJasa VARCHAR,
-    Sesi INT,
-    IdDiskon VARCHAR,
-    IdMetodeBayar UUID,
-    TglWaktu TIMESTAMP,
-    Status VARCHAR
+    NamaSubkategoriJasa VARCHAR
 ) AS $$
 BEGIN
     IF sid_param IS NOT NULL THEN
@@ -57,18 +51,9 @@ BEGIN
             tj."Id",
             tj."TglPemesanan",
             tj."TglPekerjaan",
-            tj."WaktuPekerjaan",
             tj."TotalBiaya",
-            tj."IdPelanggan",
             u."Nama",
-            tj."IdPekerja",
-            tj."IdKategoriJasa",
-            sub."NamaSubkategori",
-            tj."Sesi",
-            tj."IdDiskon",
-            tj."IdMetodeBayar",
-            ts."TglWaktu",
-            sp."Status"
+            sub."NamaSubkategori"
         FROM "TR_PEMESANAN_JASA" tj
         LEFT JOIN "TR_PEMESANAN_STATUS" ts ON tj."Id" = ts."IdTrPemesanan"
         LEFT JOIN "USER" u ON tj."IdPelanggan" = u."Id"
@@ -95,18 +80,9 @@ BEGIN
             tj."Id",
             tj."TglPemesanan",
             tj."TglPekerjaan",
-            tj."WaktuPekerjaan",
             tj."TotalBiaya",
-            tj."IdPelanggan",
             u."Nama",
-            tj."IdPekerja",
-            tj."IdKategoriJasa",
-            sub."NamaSubkategori",
-            tj."Sesi",
-            tj."IdDiskon",
-            tj."IdMetodeBayar",
-            ts."TglWaktu",
-            sp."Status"
+            sub."NamaSubkategori"
         FROM "TR_PEMESANAN_JASA" tj
         LEFT JOIN "TR_PEMESANAN_STATUS" ts ON tj."Id" = ts."IdTrPemesanan"
         LEFT JOIN "USER" u ON tj."IdPelanggan" = u."Id"
@@ -137,18 +113,9 @@ BEGIN
             tj."Id",
             tj."TglPemesanan",
             tj."TglPekerjaan",
-            tj."WaktuPekerjaan",
             tj."TotalBiaya",
-            tj."IdPelanggan",
             u."Nama",
-            tj."IdPekerja",
-            tj."IdKategoriJasa",
-            sub."NamaSubkategori",
-            tj."Sesi",
-            tj."IdDiskon",
-            tj."IdMetodeBayar",
-            ts."TglWaktu",
-            sp."Status"
+            sub."NamaSubkategori"
         FROM "TR_PEMESANAN_JASA" tj
         LEFT JOIN "TR_PEMESANAN_STATUS" ts ON tj."Id" = ts."IdTrPemesanan"
         LEFT JOIN "USER" u ON tj."IdPelanggan" = u."Id"
@@ -175,6 +142,9 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
+
+
+
 -- update_pesanan_dikerjakan
 CREATE OR REPLACE FUNCTION update_pesanan_dikerjakan(
     idTrPemesanan_param UUID,
@@ -183,9 +153,12 @@ CREATE OR REPLACE FUNCTION update_pesanan_dikerjakan(
 RETURNS VOID AS $$
 BEGIN
     -- Update the TR_PEMESANAN_STATUS table
-    UPDATE "TR_PEMESANAN_JASA" tps
-    SET "IdPekerja" = pekerjaId_param
-    WHERE tps."Id" = idTrPemesanan_param;
+    UPDATE "TR_PEMESANAN_JASA"
+    SET 
+        "IdPekerja" = pekerjaId_param,
+        "TglPekerjaan" = CURRENT_DATE,
+        "WaktuPekerjaan" = NOW() + INTERVAL '1 day' * "Sesi"
+    WHERE "Id" = idTrPemesanan_param;
 
     -- Insert a new record into the TR_PEMESANAN_STATUS table
     INSERT INTO "TR_PEMESANAN_STATUS" (
@@ -202,17 +175,251 @@ END;
 $$ LANGUAGE plpgsql;
 
 
-SELECT *
-FROM "TR_PEMESANAN_JASA" tj 
-LEFT JOIN "TR_PEMESANAN_STATUS" ts ON tj."Id" = ts."IdTrPemesanan"
-LEFT JOIN "STATUS_PESANAN" sp ON ts."IdStatus" = sp."Id"
-WHERE tj."IdPekerja" = '196b292e-d564-4508-94ce-f049e07e8688';
 
-INSERT INTO "TR_PEMESANAN_STATUS" (
-    "IdTrPemesanan",
-    "IdStatus",
-    "TglWaktu"
-) 
-VALUES 
-    ('88cb54f1-dbc2-48a6-adae-30fdc8bb1b84', 'a7634d70-d6d8-42cf-90e3-dd069bf54e33', NOW()),
-    ('88cb54f1-dbc2-48a6-adae-30fdc8bb1b84', 'e179caba-38d9-4636-ab2e-2032f5284714', NOW());
+
+-- filter_status
+CREATE OR REPLACE FUNCTION filter_status(
+    pekerjaId_param UUID,
+    searchQuery VARCHAR(255) DEFAULT NULL,
+    statusId_param UUID DEFAULT NULL
+)
+RETURNS TABLE (
+    IdTrPemesanan UUID,
+    TglPemesanan DATE,
+    TotalBiaya DECIMAL,
+    NamaPelanggan VARCHAR,
+    NamaSubkategoriJasa VARCHAR,
+    Status VARCHAR
+) AS $$
+BEGIN
+    IF statusId_param IS NULL THEN
+        RETURN QUERY
+        SELECT
+            tj."Id" AS IdTrPemesanan,
+            tj."TglPemesanan",
+            tj."TotalBiaya",
+            u."Nama" AS NamaPelanggan,
+            sub."NamaSubkategori" AS NamaSubkategoriJasa,
+            sp."Status"
+        FROM "TR_PEMESANAN_JASA" tj
+        LEFT JOIN "TR_PEMESANAN_STATUS" ts ON tj."Id" = ts."IdTrPemesanan"
+        LEFT JOIN "USER" u ON tj."IdPelanggan" = u."Id"
+        LEFT JOIN "SUBKATEGORI_JASA" sub ON tj."IdKategoriJasa" = sub."Id"
+        LEFT JOIN "STATUS_PESANAN" sp ON ts."IdStatus" = sp."Id"
+        WHERE tj."IdPekerja" = pekerjaId_param
+          AND (
+              searchQuery = '' OR
+              (
+                  (u."Nama" || ' ' || sub."NamaSubkategori" || ' ' || sp."Status") ILIKE '%' || searchQuery || '%'
+              )
+          );
+    ELSE
+        RETURN QUERY
+        SELECT
+            tj."Id" AS IdTrPemesanan,
+            tj."TglPemesanan",
+            tj."TotalBiaya",
+            u."Nama" AS NamaPelanggan,
+            sub."NamaSubkategori" AS NamaSubkategoriJasa,
+            sp."Status"
+        FROM "TR_PEMESANAN_JASA" tj
+        LEFT JOIN "TR_PEMESANAN_STATUS" ts ON tj."Id" = ts."IdTrPemesanan"
+        LEFT JOIN "USER" u ON tj."IdPelanggan" = u."Id"
+        LEFT JOIN "SUBKATEGORI_JASA" sub ON tj."IdKategoriJasa" = sub."Id"
+        LEFT JOIN "STATUS_PESANAN" sp ON ts."IdStatus" = sp."Id"
+        WHERE tj."IdPekerja" = pekerjaId_param
+          AND sp."Id" = statusId_param
+          AND (
+              searchQuery = '' OR
+              (
+                  (u."Nama" || ' ' || sub."NamaSubkategori" || ' ' || sp."Status") ILIKE '%' || searchQuery || '%'
+              )
+          );
+    END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+
+
+-- _____________________ MISC PART _____________________
+
+-- SELECT *
+-- FROM "TR_PEMESANAN_JASA" tj 
+-- LEFT JOIN "TR_PEMESANAN_STATUS" ts ON tj."Id" = ts."IdTrPemesanan"
+-- LEFT JOIN "STATUS_PESANAN" sp ON ts."IdStatus" = sp."Id"
+-- WHERE tj."IdPekerja" = '196b292e-d564-4508-94ce-f049e07e8688';
+
+-- INSERT INTO "TR_PEMESANAN_STATUS" (
+--     "IdTrPemesanan",
+--     "IdStatus",
+--     "TglWaktu"
+-- ) 
+-- VALUES 
+--     ('88cb54f1-dbc2-48a6-adae-30fdc8bb1b84', 'a7634d70-d6d8-42cf-90e3-dd069bf54e33', NOW()),
+--     ('88cb54f1-dbc2-48a6-adae-30fdc8bb1b84', 'e179caba-38d9-4636-ab2e-2032f5284714', NOW());
+
+
+
+
+
+
+
+
+
+
+
+
+-- -- filter_pemesanan
+-- CREATE OR REPLACE FUNCTION filter_pemesanan(
+--     pekerjaId_param UUID, 
+--     kid_param UUID DEFAULT NULL, 
+--     sid_param UUID DEFAULT NULL
+-- )
+-- RETURNS TABLE (
+--     IdTrPemesanan UUID,
+--     TglPemesanan DATE,
+--     TglPekerjaan DATE,
+--     WaktuPekerjaan TIMESTAMP,
+--     TotalBiaya DECIMAL,
+--     IdPelanggan UUID,
+--     NamaPelanggan VARCHAR,
+--     IdPekerja UUID,
+--     IdKategoriJasa UUID,
+--     NamaSubkategoriJasa VARCHAR,
+--     Sesi INT,
+--     IdDiskon VARCHAR,
+--     IdMetodeBayar UUID,
+--     TglWaktu TIMESTAMP,
+--     Status VARCHAR
+-- ) AS $$
+-- BEGIN
+--     IF sid_param IS NOT NULL THEN
+--         -- Case 1: Filter directly by sid
+--         RETURN QUERY
+--         SELECT
+--             tj."Id",
+--             tj."TglPemesanan",
+--             tj."TglPekerjaan",
+--             tj."WaktuPekerjaan",
+--             tj."TotalBiaya",
+--             tj."IdPelanggan",
+--             u."Nama",
+--             tj."IdPekerja",
+--             tj."IdKategoriJasa",
+--             sub."NamaSubkategori",
+--             tj."Sesi",
+--             tj."IdDiskon",
+--             tj."IdMetodeBayar",
+--             ts."TglWaktu",
+--             sp."Status"
+--         FROM "TR_PEMESANAN_JASA" tj
+--         LEFT JOIN "TR_PEMESANAN_STATUS" ts ON tj."Id" = ts."IdTrPemesanan"
+--         LEFT JOIN "USER" u ON tj."IdPelanggan" = u."Id"
+--         LEFT JOIN "SUBKATEGORI_JASA" sub ON tj."IdKategoriJasa" = sub."Id"
+--         LEFT JOIN "STATUS_PESANAN" sp ON ts."IdStatus" = sp."Id"
+--         WHERE tj."IdKategoriJasa" = sid_param
+--         AND sp."Status" = 'Mencari Pekerja Terdekat'
+--         AND NOT EXISTS (
+--             SELECT 1
+--             FROM "TR_PEMESANAN_STATUS" ts2
+--             WHERE ts2."IdTrPemesanan" = tj."Id"
+--             AND ts2."IdStatus" IN (
+--                 '57fc8243-eb71-47a5-b74d-81fba6a94f82',  -- Menunggu Pekerja Berangkat
+--                 '996980f3-cc47-4edb-a8cc-10ec02939479',  -- Pekerja tiba di lokasi
+--                 '1fe77476-1194-4de7-b7f9-72c8cdd3ef68',  -- Pelayanan jasa sedang dilakukan
+--                 '3d436422-152e-4b22-b978-49012b58e1f8',  -- Pesanan selesai
+--                 'd9521320-0b6e-4bf7-971f-b3cb7edec92f'   -- Pesanan dibatalkan
+--             )
+--         );
+--     ELSIF kid_param IS NOT NULL THEN
+--         -- Case 2: Filter by `kid` from get_category(pekerjaId)
+--         RETURN QUERY
+--         SELECT
+--             tj."Id",
+--             tj."TglPemesanan",
+--             tj."TglPekerjaan",
+--             tj."WaktuPekerjaan",
+--             tj."TotalBiaya",
+--             tj."IdPelanggan",
+--             u."Nama",
+--             tj."IdPekerja",
+--             tj."IdKategoriJasa",
+--             sub."NamaSubkategori",
+--             tj."Sesi",
+--             tj."IdDiskon",
+--             tj."IdMetodeBayar",
+--             ts."TglWaktu",
+--             sp."Status"
+--         FROM "TR_PEMESANAN_JASA" tj
+--         LEFT JOIN "TR_PEMESANAN_STATUS" ts ON tj."Id" = ts."IdTrPemesanan"
+--         LEFT JOIN "USER" u ON tj."IdPelanggan" = u."Id"
+--         LEFT JOIN "SUBKATEGORI_JASA" sub ON tj."IdKategoriJasa" = sub."Id"
+--         LEFT JOIN "STATUS_PESANAN" sp ON ts."IdStatus" = sp."Id"
+--         WHERE tj."IdKategoriJasa" IN (
+--             SELECT pk.sid
+--             FROM get_pekerja_category(pekerjaId_param) pk
+--             WHERE pk.kid = kid_param
+--         )
+--         AND sp."Status" = 'Mencari Pekerja Terdekat'
+--         AND NOT EXISTS (
+--             SELECT 1
+--             FROM "TR_PEMESANAN_STATUS" ts2
+--             WHERE ts2."IdTrPemesanan" = tj."Id"
+--             AND ts2."IdStatus" IN (
+--                 '57fc8243-eb71-47a5-b74d-81fba6a94f82',  -- Menunggu Pekerja Berangkat
+--                 '996980f3-cc47-4edb-a8cc-10ec02939479',  -- Pekerja tiba di lokasi
+--                 '1fe77476-1194-4de7-b7f9-72c8cdd3ef68',  -- Pelayanan jasa sedang dilakukan
+--                 '3d436422-152e-4b22-b978-49012b58e1f8',  -- Pesanan selesai
+--                 'd9521320-0b6e-4bf7-971f-b3cb7edec92f'   -- Pesanan dibatalkan
+--             )
+--         );
+--     ELSE
+--         -- Case 1: Filter using get_category(pekerjaId) without additional conditions
+--         RETURN QUERY
+--         SELECT
+--             tj."Id",
+--             tj."TglPemesanan",
+--             tj."TglPekerjaan",
+--             tj."WaktuPekerjaan",
+--             tj."TotalBiaya",
+--             tj."IdPelanggan",
+--             u."Nama",
+--             tj."IdPekerja",
+--             tj."IdKategoriJasa",
+--             sub."NamaSubkategori",
+--             tj."Sesi",
+--             tj."IdDiskon",
+--             tj."IdMetodeBayar",
+--             ts."TglWaktu",
+--             sp."Status"
+--         FROM "TR_PEMESANAN_JASA" tj
+--         LEFT JOIN "TR_PEMESANAN_STATUS" ts ON tj."Id" = ts."IdTrPemesanan"
+--         LEFT JOIN "USER" u ON tj."IdPelanggan" = u."Id"
+--         LEFT JOIN "SUBKATEGORI_JASA" sub ON tj."IdKategoriJasa" = sub."Id"
+--         LEFT JOIN "STATUS_PESANAN" sp ON ts."IdStatus" = sp."Id"
+--         WHERE tj."IdKategoriJasa" IN (
+--             SELECT pk.sid
+--             FROM get_pekerja_category(pekerjaId_param) pk
+--         )
+--         AND sp."Status" = 'Mencari Pekerja Terdekat'
+--         AND NOT EXISTS (
+--             SELECT 1
+--             FROM "TR_PEMESANAN_STATUS" ts2
+--             WHERE ts2."IdTrPemesanan" = tj."Id"
+--             AND ts2."IdStatus" IN (
+--                 '57fc8243-eb71-47a5-b74d-81fba6a94f82',  -- Menunggu Pekerja Berangkat
+--                 '996980f3-cc47-4edb-a8cc-10ec02939479',  -- Pekerja tiba di lokasi
+--                 '1fe77476-1194-4de7-b7f9-72c8cdd3ef68',  -- Pelayanan jasa sedang dilakukan
+--                 '3d436422-152e-4b22-b978-49012b58e1f8',  -- Pesanan selesai
+--                 'd9521320-0b6e-4bf7-971f-b3cb7edec92f'   -- Pesanan dibatalkan
+--             )
+--         );
+--     END IF;
+-- END;
+-- $$ LANGUAGE plpgsql;
+
+SELECT * FROM filter_status(
+    'f864622f-e148-4d45-8253-e31fe71754bd',
+    'Cleaning',
+    NULL
+);
--- src/pekerjaan.ts
+++ src/pekerjaan.ts
@@ -1,7 +1,96 @@
 import express from "express";
 import client from "./db";
+import { allowRoles } from "./auth";
 
 const app = express.Router();
+
+app.get("/", async (req, res) => {
+  try {
+    const pekerjaId = '196b292e-d564-4508-94ce-f049e07e8688';
+    var kategori = req.query.kategori || null; // Get kategori dari query, null jika tidak ada
+    var subkategori = req.query.subkategori || null; // Get subkategori dari query, null jika tidak ada
+    const selectedKategori = kategori
+    const selectedSubkategori = subkategori
+    // If kategori = '0' set to semua
+    if (kategori === '0'){kategori = null};
+    if (subkategori === '0'){subkategori = null};
+
+    const { rows: pekerjaKategori } = await client.query(
+      "SELECT * FROM get_pekerja_category($1)",
+      [pekerjaId]
+    );
+    
+    const pekerjaKategoriDict = transformKategoriData(pekerjaKategori);
+  
+    const { rows: orderResult } = await client.query(
+      "SELECT * FROM filter_pemesanan($1, $2, $3)",
+      [pekerjaId, kategori, subkategori]
+    );
+
+    res.render("pekerjaan/main", {
+      message: req.query.message || "",
+      pekerjaKategoriDict,
+      pekerjaKategoriDictJson: JSON.stringify(pekerjaKategoriDict),
+      orderResult,
+      selectedKategori,
+      selectedSubkategori,
+    });
+  } catch(error) {
+    console.error("Error fetching data:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.post("/kerjakan", async (req, res) => {
+  try {
+    const pekerjaId = 'f864622f-e148-4d45-8253-e31fe71754bd';
+    const { idtrpemesanan: idTrPemesanan } = req.body;
+
+    await client.query(
+      "SELECT update_pesanan_dikerjakan($1, $2)",
+      [idTrPemesanan, pekerjaId]
+    );
+    res.redirect("/pekerjaan?message=Success");
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/status", async (req, res) => {
+  try{
+    const pekerjaId = 'f864622f-e148-4d45-8253-e31fe71754bd';
+    const searchQuery = req.query.searchQuery || '';
+    const status = req.query.status || null;
+
+    const { rows: statusResult } = await client.query(
+      "SELECT * FROM filter_status($1, $2, $3)",
+      [pekerjaId, searchQuery, status]
+    );
+    res.render("pekerjaan/status", {
+      message: req.query.message || "",
+      statusResult
+    });
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.post("/status/update-status", async (req, res) => {
+  try {
+    const pekerjaId = 'f864622f-e148-4d45-8253-e31fe71754bd';
+
+    res.redirect("/pekerjaan/status?message=Success");
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+export default app;
+
+// ___________________ UTILITY FUNCTION ___________________
 
 // Fungsi untuk reshape data kategori ke dalam bentuk map
 const transformKategoriData = (data: any[]) => {
@@ -24,64 +113,3 @@ const transformKategoriData = (data: any[]) => {
     return result;
   }, []);
 };
-
-
-app.get("/", async (req, res) => {
-  const pekerjaId = '196b292e-d564-4508-94ce-f049e07e8688';
-  var kategori = req.query.kategori || null; // Get kategori dari query, null jika tidak ada
-  var subkategori = req.query.subkategori || null; // Get subkategori dari query, null jika tidak ada
-  const selectedKategori = kategori
-  const selectedSubkategori = subkategori
-
-  // If kategori = '0' set to semua
-  if (kategori === '0'){kategori = null};
-  if (subkategori === '0'){subkategori = null};
-
-  
-  try {
-    const { rows: pekerjaKategori } = await client.query(
-      "SELECT * FROM get_pekerja_category($1)",
-      [pekerjaId]
-    );
-    
-    const pekerjaKategoriDict = transformKategoriData(pekerjaKategori);
-  
-    const { rows: orderResult } = await client.query(
-      "SELECT * FROM filter_pemesanan($1, $2, $3)",
-      [pekerjaId, kategori, subkategori]
-    );
-    res.render("pekerjaan/main", {
-      pekerjaKategoriDict,
-      pekerjaKategoriDictJson: JSON.stringify(pekerjaKategoriDict),
-      orderResult,
-      selectedKategori,
-      selectedSubkategori,
-    });
-  } catch(error) {
-    console.error("Error fetching data:", error);
-    res.status(500).send("Internal Server Error");
-  }
-});
-
-app.post("/kerjakan", async (req, res) => {
-  const pekerjaId = '196b292e-d564-4508-94ce-f049e07e8688';
-  const { idtrpemesanan: idTrPemesanan } = req.body;
-  try {
-    await client.query(
-      "SELECT update_pesanan_dikerjakan($1, $2)",
-      [idTrPemesanan, pekerjaId]
-    )
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    res.status(500).send("Internal Server Error");
-    res.redirect("/pekerjaan");
-  }
-
-
-});
-
-app.get("/status", (req, res) => {
-  res.render("pekerjaan/status");
-});
-
-export default app;
