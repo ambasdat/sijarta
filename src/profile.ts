a7e8275653993c732d7abf33d7ef817bbc3929aa
secona <77039267+secona@users.noreply.github.com>
2024-12-06 21:43:30 +0700

    feat: error handling when updating user profile

--- src/profile.ts
+++ src/profile.ts
@@ -36,70 +36,96 @@ app.get("/:userId", async (req, res) => {
   return res.render("404");
 })
 
-app.post("/pekerja/:pekerjaId", allowRoles(['pekerja']), (req, res) => {
-  const pekerjaId = req.params.pekerjaId;
+app.post("/:userId", allowRoles(['pekerja', 'pengguna']), async (req, res) => {
+  const userId = req.params.userId;
 
-  if (req.userId != pekerjaId) {
+  if (req.userId != userId) {
     return res.redirect("/home");
   }
 
-  client.query(
-    `SELECT update_profile_pekerja(
-       $1::UUID,    -- pekerjaId
-       $2::VARCHAR, -- nama
-       $3::CHAR(1), -- jenis kelamin
-       $4::VARCHAR, -- nomor hp
-       $5::DATE,    -- tanggal lahir
-       $6::VARCHAR, -- alamat
-       $7::VARCHAR, -- nama bank
-       $8::VARCHAR, -- nomor rekening
-       $9::VARCHAR, -- npwp
-       $10::VARCHAR -- link foto
-     );`,
-    [
-      pekerjaId,
-      req.body.nama,
-      req.body.jenisKelamin,
-      req.body.phone,
-      req.body.dob,
-      req.body.address,
-      req.body.bank,
-      req.body.norek,
-      req.body.npwp,
-      req.body.url,
-    ]
-  );
+  if (req.userType === 'pekerja') {
+    try {
+      await client.query(
+        `SELECT update_profile_pekerja(
+           $1::UUID,    -- pekerjaId
+           $2::VARCHAR, -- nama
+           $3::CHAR(1), -- jenis kelamin
+           $4::VARCHAR, -- nomor hp
+           $5::DATE,    -- tanggal lahir
+           $6::VARCHAR, -- alamat
+           $7::VARCHAR, -- nama bank
+           $8::VARCHAR, -- nomor rekening
+           $9::VARCHAR, -- npwp
+           $10::VARCHAR -- link foto
+         );`,
+        [
+          userId,
+          req.body.nama,
+          req.body.jenisKelamin,
+          req.body.phone,
+          req.body.dob,
+          req.body.address,
+          req.body.bank,
+          req.body.norek,
+          req.body.npwp,
+          req.body.url,
+        ]
+      );
+    } catch (err: any) {
+      const pekerja = await client.query(
+        `SELECT * FROM "USER" u JOIN "PEKERJA" p ON p."Id" = u."Id" WHERE u."Id" = $1`,
+        [userId]
+      );
 
-  res.redirect(`/profile/${pekerjaId}`);
-});
+      if (pekerja.rowCount != null && pekerja.rowCount > 0) {
+        const p = pekerja.rows[0];
+        return res.render("profile/pekerja", {
+          pekerja: p,
+          error: err.message,
+          isError: true,
+          isSelf: req.userId && p.Id === req.userId,
+        });
+      }
+    }
+  } else if (req.userType === 'pengguna') {
+    try {
+      await client.query(
+        `SELECT update_profile_pelanggan(
+           $1::UUID,    -- pekerjaId
+           $2::VARCHAR, -- nama
+           $3::CHAR(1), -- jenis kelamin
+           $4::VARCHAR, -- nomor hp
+           $5::DATE,    -- tanggal lahir
+           $6::VARCHAR  -- alamat
+         );`,
+        [
+          userId,
+          req.body.nama,
+          req.body.jenisKelamin,
+          req.body.phone,
+          req.body.dob,
+          req.body.address,
+        ]
+      );
+    } catch (err: any) {
+      const pelanggan = await client.query(
+        `SELECT * FROM "USER" u JOIN "PELANGGAN" p ON p."Id" = u."Id" WHERE u."Id" = $1`,
+        [userId]
+      );
 
-app.post("/pengguna/:penggunaId", (req, res) => {
-  const penggunaId = req.params.penggunaId;
-
-  if (req.userId != penggunaId) {
-    return res.redirect("/home");
+      if (pelanggan.rowCount != null && pelanggan.rowCount > 0) {
+        const p = pelanggan.rows[0];
+        return res.render("profile/pengguna", {
+          pelanggan: p,
+          error: err.message,
+          isError: true,
+          isSelf: req.userId && p.Id === req.userId,
+        });
+      }
+    }
   }
 
-  client.query(
-    `SELECT update_profile_pelanggan(
-       $1::UUID,    -- pekerjaId
-       $2::VARCHAR, -- nama
-       $3::CHAR(1), -- jenis kelamin
-       $4::VARCHAR, -- nomor hp
-       $5::DATE,    -- tanggal lahir
-       $6::VARCHAR  -- alamat
-     );`,
-    [
-      penggunaId,
-      req.body.nama,
-      req.body.jenisKelamin,
-      req.body.phone,
-      req.body.dob,
-      req.body.address,
-    ]
-  );
-
-  res.redirect(`/profile/${penggunaId}`);
+  res.redirect(`/profile/${userId}`);
 });
 
 export default app;
