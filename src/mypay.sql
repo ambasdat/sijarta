12292ed9cc703bbc3d0da79c1fc4a495d23ed24d
adlynfakhreyz <adlynuniacc@gmail.com>
2024-12-08 10:11:11 +0700

    fix: change rupiah format

--- src/mypay.sql
+++ src/mypay.sql
@@ -43,7 +43,10 @@ RETURNS TABLE(
 ) AS $$
 BEGIN
     RETURN QUERY
-    SELECT u."Nama", TO_CHAR(u."SaldoMyPay", 'FM9,999,999,999')::VARCHAR AS "SaldoMyPay", u."NoHP"
+    SELECT 
+        u."Nama", 
+        REPLACE(TO_CHAR(u."SaldoMyPay", 'FM9,999,999,999'), ',', '.')::VARCHAR AS "SaldoMyPay", 
+        u."NoHP"
     FROM "USER" u
     WHERE u."Id" = userId;
 END;
@@ -61,10 +64,13 @@ RETURNS TABLE (
 ) AS $$
 BEGIN
     RETURN QUERY
-    SELECT TO_CHAR(t."Nominal", 'FM9,999,999,999')::VARCHAR AS "Nominal", t."Tgl", k."Nama"
+    SELECT 
+        REPLACE(TO_CHAR(t."Nominal", 'FM9,999,999,999'), ',', '.')::VARCHAR AS "Nominal", 
+        t."Tgl", 
+        k."Nama"
     FROM "TR_MYPAY" t
     LEFT JOIN "KATEGORI_TR_MYPAY" k 
-    ON k."Id" = t."KategoriId"
+        ON k."Id" = t."KategoriId"
     WHERE t."UserId" = userId;
 END;
 $$ LANGUAGE plpgsql;
